2521416961ea8d5f028409dbe9b875aeaff99546
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-06-05 10:10:13 -0300

    ajuste inicial

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
-    try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
-        }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
+console.log("mandando oi pro amigo!");
 
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
-
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
-        console.log(erro);
-    }
+function mandaMensagem() {
+    console.log('tudo bem?');
+    console.log('vou te mandar uma solicitação');
+    console.log('solicitação recebida!');
 }
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+mandaMensagem()
+
+console.log('tchau tchau')
