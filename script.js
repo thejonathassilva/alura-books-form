0eaf00c4b3ed19642ba354ef4047e46c3d3ec000
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-06-19 09:06:19 -0300

    tratamentos básicos

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+let searchCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(response => response.json())
+    .then(r => {
+        if(r.erro) {
+            throw Error('Esse cep não existe!');
+        } else {
+            console.log(r)
+        }
+    })
+    .catch(error => console.log(error))
+    .finally(message => console.log('Processamento concluido!'));
 
-console.log(consultaCEP)
+
+console.log(searchCEP)
